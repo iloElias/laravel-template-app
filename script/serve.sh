5437f0f752f3aae310b86924a752ddbd1b004000
iloElias <murilo7456@gmail.com>
2026-05-20 16:52:55 -0300

    fix: Adicionar opção --no-reload ao comando de inicialização do servidor Laravel

--- script/serve.sh
+++ script/serve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "CI  Starting Laravel production server"
-php artisan serve &
+php artisan serve --no-reload &
 
 echo "CI  Starting Laravel Reverb server"
 php artisan reverb:start &
