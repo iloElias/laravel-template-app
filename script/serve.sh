27e6376291fe674ef8ac5b8b673a83d34164694b
iloElias <murilo7456@gmail.com>
2026-05-20 18:44:18 -0300

    refactor: changed host ip for artisan serve

--- script/serve.sh
+++ script/serve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "CI  Starting Laravel production server."
-php artisan serve --port=80 --no-reload &
+php artisan serve --host=0.0.0.0 --port=80 --no-reload &
 
 # echo "CI  Starting Laravel Reverb server."
 # php artisan reverb:start --port=6001 &
