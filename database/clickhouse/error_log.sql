bc39ddc6059dc0180d69957e7b8cecc913d756e1
iloElias <murilo7456@gmail.com>
2026-05-20 13:57:27 -0300

    fix: Remover espaços desnecessários na definição das tabelas error_log e request_history

--- database/clickhouse/error_log.sql
+++ database/clickhouse/error_log.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS error_log (
-    url Nullable (String),
+    url Nullable(String),
     error_message String,
-    stack_trace Nullable (String),
-    request_data Nullable (String),
+    stack_trace Nullable(String),
+    request_data Nullable(String),
     created_at DateTime DEFAULT now()
-) ENGINE = MergeTree ()
+) ENGINE = MergeTree()
 ORDER BY (created_at);
