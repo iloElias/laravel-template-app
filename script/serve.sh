db357cf59e68cff4599bd1399e4ddb288dc6c017
iloElias <murilo7456@gmail.com>
2026-05-20 17:52:03 -0300

    fix: Comentar inicialização do servidor Reverb no script de inicialização

--- script/serve.sh
+++ script/serve.sh
@@ -3,7 +3,7 @@
 echo "CI  Starting Laravel production server."
 php artisan serve --port=8000 --no-reload &
 
-echo "CI  Starting Laravel Reverb server."
-php artisan reverb:start --port=6001 &
+# echo "CI  Starting Laravel Reverb server."
+# php artisan reverb:start --port=6001 &
 
 wait -n
