f86ef0c3c28433b03217781809f61399d841e015
iloElias <murilo7456@gmail.com>
2026-05-20 16:53:49 -0300

    fix: Adicionar opções de host e porta ao comando de inicialização do servidor Laravel

--- script/serve.sh
+++ script/serve.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "CI  Starting Laravel production server"
-php artisan serve --no-reload &
+php artisan serve --host=0.0.0.0 --port=8000 --no-reload &
 
 echo "CI  Starting Laravel Reverb server"
-php artisan reverb:start &
+php artisan reverb:start --host=0.0.0.0 --port=6001 &
 
 wait -n
