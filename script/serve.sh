215ecf72f0aab6717e1647b281b38dda2dafaaed
iloElias <murilo7456@gmail.com>
2026-05-20 18:36:55 -0300

    fix: Atualizar configuração do Nginx e script de inicialização para corrigir portas e otimizar conexões

--- script/serve.sh
+++ script/serve.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "CI  Starting Laravel production server."
-php artisan serve --port=8000 --no-reload &
+php artisan serve --port=80 --no-reload &
 
 # echo "CI  Starting Laravel Reverb server."
 # php artisan reverb:start --port=6001 &
